cec00ad6a3f4c66c9e2783a01fd35825cbc811a9
Nate Kong <NateWKong@gmail.com>
2015-02-05 15:56:32 -0800

    first commit

--- Assignment2/src/SRT.java
+++ Assignment2/src/SRT.java
@@ -1,13 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- *
- * @author Standard
+ * Shortest Remaining time
+ * @author Nate Kong
  */
+
 public class SRT {
+    private List<Process> srt;
+    
+    public SRT(ArrayList<Process> p) {
+    	this.srt = p;
+    }
+    
+    
+    
     
 }
