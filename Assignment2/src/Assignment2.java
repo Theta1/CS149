401468e053a04c26451f6a912169eebb6119d501
Nate Kong <NateWKong@gmail.com>
2015-02-08 20:51:51 -0800

    added multiple arrays

--- Assignment2/src/Assignment2.java
+++ Assignment2/src/Assignment2.java
@@ -3,7 +3,9 @@ import java.util.List;
 
 
 /**
- *
+ * Runs the different scheduling algorithms
+ * for processes. Calculates statistics for
+ * the scheduling
  * @author David-Eric Thorpe
  */
 public class Assignment2 {
@@ -14,11 +16,30 @@ public class Assignment2 {
      */
     public static void main(String[] args) {
 	
-	List <Process>srt = new ArrayList <Process>();
-	
-	for(int i=0;i<30;i++){
-	    Process n = new Process(i);
-	    srt.add(n);
-	}
+		List<Process> list1 = new ArrayList <Process>();
+		List<Process> list2 = new ArrayList <Process>();
+		List<Process> list3 = new ArrayList <Process>();
+		List<Process> list4 = new ArrayList <Process>();
+		List<Process> list5 = new ArrayList <Process>();
+		
+		
+		for(int i=0; i<150; i++)
+		{
+		    Process n = new Process(i);
+		    if (i < 30)
+		    {   list1.add(n); }
+		    else if (i>=30 && i<60)
+		    {	list2.add(n);	}
+		    else if (i>=60 && i<90)
+		    {	list3.add(n);	}
+		    else if (i>=90 && i<120)
+		    {	list4.add(n);	}
+		    else
+		    {	list5.add(n);	}
+		}
+		
+		
+		
+		
     }    
 }
