6f8624b07c3f74daafca68b9b913ddc033f9a755
siebenluke <siebenluke@gmail.com>
2015-02-12 15:27:53 -0800

    added actualStartTime, and turnAroundTime along with other methods for statistic calculations; sorted methods alphabetically

--- Assignment2/src/Process.java
+++ Assignment2/src/Process.java
@@ -7,11 +7,13 @@ import java.util.Random;
  * Team: Inception
  ***********************************************/
 public class Process  {
+    private String name;
     private float arrivalTime;
     private float runTime;
     private int priority;
-    private String name;
     private int quantumWait;
+    private int actualStartTime;
+    private int turnAroundTime;
 
     /**
      * Default Constructor
@@ -19,10 +21,16 @@ public class Process  {
      * be first for any of the scheduling algorithms
      */
     public Process() {
-        this.name = "N/A";
-        this.arrivalTime = 200;
-        this.runTime = 1000;
-        this.priority = -1;
+        name = "N/A";
+
+        arrivalTime = 200;
+        runTime = 1000;
+        priority = -1;
+
+        quantumWait = 0;
+
+        actualStartTime = -1;
+        turnAroundTime = -1;
     }
 
     /**
@@ -32,10 +40,32 @@ public class Process  {
     public Process(int n) {
         name = n + "";
 
-        Random rn = new Random();
-        arrivalTime = rn.nextFloat() * 99.0f;
-        runTime = rn.nextFloat() * 10.0f;
-        priority = rn.nextInt(4) + 1; // gets a random number from 1 to 4
+        // random stats
+        Random random = new Random();
+        arrivalTime = random.nextFloat() * 99.0f; // gets a random float from 0.0 to 99.0
+        runTime = random.nextFloat() * 10.0f; // gets a random float from 0.0 to 10.0
+        priority = random.nextInt(4) + 1; // gets a random integer from 1 to 4
+
+        quantumWait = 0; // how long a process has been waiting in a queue
+
+        actualStartTime = -1; // the first time a process starts running
+        turnAroundTime = -1; // the last time a process runs
+    }
+
+    /**
+     * Gets the actual actualStartTime.
+     * @return the actualStartTime
+     */
+    public int getActualStartTime() {
+        return actualStartTime;
+    }
+
+    /**
+     * Returns the arrival time of the float.
+     * @return a float representing the arrival time for the process.
+     */
+    public float getArrivalTime(){
+        return arrivalTime;
     }
 
     /**
@@ -45,17 +75,29 @@ public class Process  {
     public String getName() {
         return name;
     }
-    
+
+    /**
+     * Returns the priority of the process where the larger number represents the higher relative priority.
+     * @return an integer representing the priority of the processes
+     */
+    public int getPriority(){
+        return priority;
+    }
+
+    /**
+     * Gets the quantumWait.
+     * @return the quantumWait
+     */
     public int getQuantumWait(){
     	return quantumWait;
     }
-    
+
     /**
-     * Returns the arrival time of the float. 
-     * @return a float representing the arrival time for the process.
+     * Gets the responseTime.
+     * @return the responseTime
      */
-    public float getArrivalTime(){
-        return arrivalTime;
+    public int getResponseTime() {
+        return Math.round(arrivalTime + 0.5f) - actualStartTime;
     }
     
     /**
@@ -65,50 +107,59 @@ public class Process  {
     public float getRunTime(){
         return runTime;
     }
-    
+
     /**
-    * Returns the priority of the process where the larger number represents the higher relative priority.
-    * @return an integer representing the priority of the processes  
-    */
-    public int getPriority(){
-        return priority;
+     * Gets the turnAroundTime.
+     * @return the turnAroundTime
+     */
+    public int getTurnAroundTime() {
+        return turnAroundTime;
+    }
+
+    /**
+     * Gets the waitingTime.
+     * @return the waitingTime.
+     */
+    public int getWaitingTime() {
+        return turnAroundTime - Math.round(runTime + 0.5f);
+    }
+
+    /**
+     * Sets the actual actualStartTime.
+     * @param actualStartTime the actualStartTime
+     */
+    public void setActualStartTime(int actualStartTime) {
+        if(this.actualStartTime < 0) {
+            this.actualStartTime = actualStartTime;
+        }
+    }
+
+    /**
+     * Sets the actual turnAroundTime which is the last time the process ran.
+     * @param turnAroundTime the turnAroundTime
+     */
+    public void setTurnAroundTime(int turnAroundTime) {
+        this.turnAroundTime = turnAroundTime;
+    }
+
+    /**
+     * Decrements the runTime by one.
+     */
+    public void decrementRunTime() {
+        runTime--;
     }
 
 	/**
-	 * DEPRECATED
-	 * Sets the arrival time of the process
-	 * @param arrivalTime the arrivalTime as a float
-	 */
-	public void setArrivalTime(float arrivalTime) {
-		this.arrivalTime = arrivalTime;
-	}
-
-	/**
-	 * Increases the variable by one. 
+	 * Increments the quantumWait by one..
 	 */
 	public void incrementQuantumWait(){
 		quantumWait++;
 	}
 
 	/**
-	 * Sets the run time of the process one smaller
-	 */
-	public void decrementRunTime() {
-		this.runTime--;
-	}
-
-	/**
-	 * Sets the priority of the process by one higher
+	 * Increments the priority by one.
 	 */
 	public void incrementPriority() {
-		this.priority++;
-	}
-
-	/**
-	 * Sets the name of the process
-	 * @param name the name
-	 */
-	public void setName(String name) {
-		this.name = name;
+		priority++;
 	}
 }
