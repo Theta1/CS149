170c798868176d81612f923bae1c19be12b32a93
De.Thorpe <cs.temp.mail@gmail.com>
2015-02-12 00:21:38 -0800

    Simplied test

--- Assignment2/src/TestProcess.java
+++ Assignment2/src/TestProcess.java
@@ -17,30 +17,11 @@ public class TestProcess {
      * @param args
      */
     public static void main(String[] args) {
-	ArrayList<Process> list1 = new ArrayList <Process>();
-	ArrayList<Process> list2 = new ArrayList <Process>();
-	ArrayList<Process> list3 = new ArrayList <Process>();
-	ArrayList<Process> list4 = new ArrayList <Process>();
-	ArrayList<Process> list5 = new ArrayList <Process>();
-	
-	
-	for(int i=0; i<150; i++)
-	{
-	    Process n = new Process(i);
-	    if (i < 30)
-	    {   list1.add(n); }
-	    else if (i>=30 && i<60)
-	    {	list2.add(n);	}
-	    else if (i>=60 && i<90)
-	    {	list3.add(n);	}
-	    else if (i>=90 && i<120)
-	    {	list4.add(n);	}
-	    else
-	    {	list5.add(n);	}
-	}
+
 	for(int i=0; i<30; i++)
 	{
-	System.out.println(list1.get(i).getArrivalTime());
+	    Process n = new Process(i);
+	    System.out.println(n.getArrivalTime());
 	}
     }
 
