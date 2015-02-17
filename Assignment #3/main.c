7e620cf2a1cf90e6638a2595d6f0efc19046be8c
De.Thorpe <cs.temp.mail@gmail.com>
2015-02-17 14:40:30 -0800

    added file comment

--- Assignment #3/main.c
+++ Assignment #3/main.c
@@ -1,5 +1,14 @@
 #include "stdio.h"
 
+
+/*********************************************
+ * Runs the class enrollment algorithms
+ * using multithreading.
+ * Calculates statistics form runtime
+ * @author Team Theta 1: David-Eric Thorpe, Nathan Kong, Luke Sieben, Dennis Hsu
+ * CS 149
+ **********************************************/
+
 /**
 * Main method
 */
