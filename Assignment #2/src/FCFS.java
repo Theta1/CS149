27ad9bd3343780e3ebd7dd20e369e4d529e8cb28
De.Thorpe <cs.temp.mail@gmail.com>
2015-02-16 16:25:49 -0800

    renamed Assignment folder

---
+++ Assignment #2/src/FCFS.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+/**********************************************
+ * First Come First Serve
+ * 
+ * Each process is ran as in the
+ * order it comes in at
+ * 
+ * @author Team: Inception
+ * CS149
+ * 
+ *********************************************/
+public class FCFS implements SchedulerInterface{
+    private static final int QUANTUM_MAX = 100;
+    private ArrayList<Process> processList;
+    private ArrayList<String> stringList;
+    private ArrayList<Process> stats;
+
+    /**
+     * Creates a First Come First Serve schedule.
+     * @param processList the process list
+     */
+    public FCFS(ArrayList<Process> processList){
+        this.processList = processList;
+        stringList = new ArrayList<String>();
+        stats = new ArrayList<Process>();
+
+        run();
+    }
+
+    /**
+     * Runs the FCFS algorithm.
+     */
+    private void run() {
+        int quantum = 0;
+
+        while(!processList.isEmpty() && quantum <= QUANTUM_MAX) {
+            Process process = processList.remove(0);
+
+            // idle time
+            while(process.getArrivalTime() > quantum) {
+                stringList.add("");
+
+                quantum++;
+            }
+
+            // process time
+            process.setActualStartTime(quantum);
+            while(process.getRunTime() > 0) {
+                stringList.add(process.getName());
+
+                // update process stats
+                process.decrementRunTime();
+                process.incrementQuantaTime();
+                process.setEndTime(quantum);
+
+                quantum++;
+            }
+            stats.add(process);
+        }
+    }
+
+    public ArrayList<String> getStringList() {
+        return stringList;
+    }
+    
+    public ArrayList<Process> getStats() {
+        return stats;
+    }
+}
