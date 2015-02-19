bab08e661192612865abd097464530969c06da54
Nathan Kong <nate@debianNK.sjsu.edu>
2015-02-19 15:24:35 -0800

    first

---
+++ Assignment #3/Student.c
@@ -0,0 +1,25 @@
+/**************************************
+A structure to create a student
+
+*************************************/
+#include <stdio.h>
+
+typedef struct {
+    int arriveTime;
+    int finishTime;
+    char[3] priority;
+    int section;
+    int id;
+} STUDENT;
+
+/*
+Figures out if a student can enter a section
+*/
+int canTakeSection( STUDENT* student) {
+    if (student->section == )
+}
+
+
+/*
+put print functions in here
+*/
