f25b2cc50492aee4a31fdb74aba21394046088c3
DT <de.thorpe@gmail.com>
2015-02-19 15:46:08 -0800

    init

---
+++ Assignment #3/Student.c
@@ -0,0 +1,25 @@
+/**************************************
+A structure to create a student
+Student functions
+print functions
+*************************************/
+#include "Student.h"
+
+
+/*
+Figures out if a student can enter a section
+*/
+/*
+int canTakeSection( STUDENT* student) {
+    //if (student->section == )
+    return student->section;
+}
+*/
+
+/*
+put print functions in here
+*/
+
+void printme() {
+    printf("hello world");
+}
