5c904d8a550bd1519771dee097a5efa83b3503ab
y510p <y510p@deansassist-win.sjsu.edu>
2015-02-22 20:08:12 -0800

    added canEnroll

--- Assignment #3/Student.c
+++ Assignment #3/Student.c
@@ -23,11 +23,26 @@ int getTurnAroundTime(STUDENT student) {
 }
 
 /**
+  * Checks if a student can enroll in a section.
+  * @param student the student
+  * @param section the section the student is trying to add
+  * @return 1 if the student can enroll in that section
+  *         0 if the student cannot enroll in that section
+  */
+int canEnroll(STUDENT student, int section) {
+    if(student.section == 0) {
+        return 1;
+    }
+
+    return student.section == section;
+}
+
+/**
   * Checks if a student is impatient (waited 10s in the queue) or not.
   * @param student the student
   * @param processStartTime the time the student is about to be processed in
   * @return 1 if the student is impatient
-            0 if the student is not impatient
+  *         0 if the student is not impatient
 */
 int isImpatient(STUDENT student, int processStartTime) {
     return processStartTime - student.arriveTime >= 10;
