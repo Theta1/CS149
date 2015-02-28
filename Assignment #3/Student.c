c35009d10d3cc1751e8360297db6e20fdc713cc7
siebenluke <siebenluke@gmail.com>
2015-02-27 18:03:46 -0800

    Update Student.c

--- Assignment #3/Student.c
+++ Assignment #3/Student.c
@@ -59,13 +59,3 @@ int isPriority(STUDENT student, char *priority) {
     return strcmp(student.priority, priority) == 0;
 }
 
-/**
-* Gets the current time
-* @return the current time
-* in seconds as an int
-*/
-int getTime() {
-    time_t current_time;
-    time(&current_time);
-    return current_time;
-}
