6d82bb029cf5f1db303e471c8d3f79851c4a6c6d
DT <de.thorpe@gmail.com>
2015-03-12 15:44:11 -0700

    Created random generator

--- Assignment #4 paging/src/RandomPick.java
+++ Assignment #4 paging/src/RandomPick.java
@@ -1,5 +1,26 @@
+import java.util.Random;
 
-public class RandomPick
-{
+
+/**
+ * Called with an int and returns an int based on the assignment specifications.
+ * @author Theta(1)
+ *
+ */
+public class RandomPick{
+	private final static int vMem = 10;
+	private final static int LOCALITY_PROBABILITY = 7;
+	public static int pickAPage(int i){
+		Random random = new Random();
+		int probability = random.nextInt(vMem);
+		int locality;
+		if (probability < LOCALITY_PROBABILITY)
+			locality = random.nextInt((1 - -1) + 1) + 1;
+		else 
+			locality = random.nextInt((8 - 2) + 1) + 2;
+		return (i+locality)%10;
+	}
+	
 
 }
+
+
