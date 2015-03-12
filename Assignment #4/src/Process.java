61adb80b7542081f8c448dba2043a428eefe5eec
Nate Kong <NateWKong@gmail.com>
2015-03-12 13:41:13 -0700

    first commit

---
+++ Assignment #4/src/Process.java
@@ -0,0 +1,15 @@
+
+public class Process {
+	int size;
+	int duration;
+	String name;
+	
+	Process(String a) {
+		size =
+		duration = 
+		name = 
+	}
+	
+	
+	
+}
---
+++ Assignment #4/src/Swapping.java
@@ -0,0 +1,10 @@
+
+public class Swapping {
+
+	public static void main(String[] args) {
+
+		
+
+	}
+
+}
