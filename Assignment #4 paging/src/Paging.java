8bd6ecc56d7ce504304b668ba78891a4c011f7d8
Dennis <denniseh7@gmail.com>
2015-03-12 14:42:01 -0700

    Testing

--- Assignment #4 paging/src/Paging.java
+++ Assignment #4 paging/src/Paging.java
@@ -5,6 +5,7 @@ public class Paging {
 		int vMem = 10;
 		int hardMem = 4;
 		
+		//run each 5 times, get average hit ratio for printing
 		FIFO fifo = new FIFO(vMem, hardMem);
 		int h1 = fifo.run; //returns the hit ratio
 		
