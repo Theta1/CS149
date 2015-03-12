815c55077a94edddfe8cd4c80c2ad7b856720988
DT <de.thorpe@gmail.com>
2015-03-12 15:08:17 -0700

    Created basic main method. Should not require change in development.

--- Assignment #4 paging/src/Paging.java
+++ Assignment #4 paging/src/Paging.java
@@ -5,15 +5,23 @@ public class Paging {
 		int vMem = 10;
 		int hardMem = 4;
 		
-		//run each 5 times, get average hit ratio for printing
+		//run each 5 times
 		FIFO fifo = new FIFO(vMem, hardMem);
-		int h1 = fifo.run; //returns the hit ratio
+		LFU lfu = new LFU(vMem, hardMem);
+		LRU lru = new LRU(vMem, hardMem);
+		MFU mfu = new MFU(vMem, hardMem);
 		
+		//returns the hit ratio
+		int h1 = fifo.run(); 
+		int h2 = lfu.run(); 
+		int h3 = lru.run(); 
+		int h4 = mfu.run(); 
 		
-		
-		
-		
-		//print average hit ratio per 
+		//print average hit ratio
+		System.out.println("--Average hit ratios--");
+		System.out.println("FIFO: "+h1);
+		System.out.println("LFO:  "+h2);
+		System.out.println("LRU:  "+h3);
+		System.out.println("MFU:  "+h4);
 	}
-
 }
