eaa3541e23c6fde4ddd76692a3551c3bb8985821
DT <de.thorpe@gmail.com>
2015-03-12 19:44:51 -0700

    Minor corrections to comments and print format

--- Assignment #4/src/Print.java
+++ Assignment #4/src/Print.java
@@ -10,15 +10,16 @@ public class Print {
 	
 	/**
 	 * Each time a process is swapped in, or a process completes and therefore
-	 * is removed from memory, print a memory map, e.g., AAAAA....BBBBBBBB..CCCC
+	 * is removed from memory, print a memory map, e.g., [ 1][ 1][ 1][ 1][ 1][..]
+	 * [..][..][..][10][10][10][10][10][..][ 3][ 3][ 3]
 	 * where the characters are the process names (one character per MB) and the
-	 * dots are holes (one per MB). Indicate which process entered or left. For
+	 * dots are holes ([..] per MB). Indicate which process entered or left. For
 	 * an entering process, also print its size and duration.
 	 * 
 	 * @param memory
 	 *            is an int array that represents the processes in memory.
 	 * @param process
-	 *            is the process number that has been added or removed
+	 *            is the process that has been added or removed.
 	 */
 	void printMap(ArrayList<Integer> mem, Process process) {
 		boolean processAdded = false;
@@ -28,10 +29,10 @@ public class Print {
 			if (i == mem.get(mem.size()-1))
 			{	System.out.println(i + "]");	}
 			else if (i == 0)
-			{	System.out.print(".");	}
+			{	System.out.print("..");	}
 			else
 			{	
-				System.out.print(i);
+				System.out.printf("%2d",i);
 				if (i == process.getName()){
 					processAdded = true;
 				}
