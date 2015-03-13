788f2bc8ea09d57c5fbc7972eb37907b48e8f9f5
DT <de.thorpe@gmail.com>
2015-03-12 19:37:10 -0700

    Improved print function to meet prompt

--- Assignment #4/src/Print.java
+++ Assignment #4/src/Print.java
@@ -9,10 +9,19 @@ public class Print {
 
 	
 	/**
+	 * Each time a process is swapped in, or a process completes and therefore
+	 * is removed from memory, print a memory map, e.g., AAAAA....BBBBBBBB..CCCC
+	 * where the characters are the process names (one character per MB) and the
+	 * dots are holes (one per MB). Indicate which process entered or left. For
+	 * an entering process, also print its size and duration.
 	 * 
-	 * @param mem the memory map
+	 * @param memory
+	 *            is an int array that represents the processes in memory.
+	 * @param process
+	 *            is the process number that has been added or removed
 	 */
-	void printMap(ArrayList<Integer> mem) {
+	void printMap(ArrayList<Integer> mem, Process process) {
+		boolean processAdded = false;
 		System.out.print("[");
 		for(int i: mem)
 		{	
@@ -21,7 +30,19 @@ public class Print {
 			else if (i == 0)
 			{	System.out.print(".");	}
 			else
-			{	System.out.print(i);	}
+			{	
+				System.out.print(i);
+				if (i == process.getName()){
+					processAdded = true;
+				}
+			}
+		}
+		if (processAdded){
+			System.out.printf(" Added: %2d", process.getName());
+			System.out.println(" of size " + process.getSize() + " and durration "
+					+ process.getDuration());
+		}else{
+			System.out.printf(" Removed: %2d\n", process.getName());
 		}
 	}
 }
