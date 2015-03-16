0c9e655c19c8cc1ce6976e0e79530f9e90fcd76d
David <de.thorpe@gmail.com>
2015-03-16 14:25:50 -0700

    Added comments

--- Assignment #4 paging/src/RandomPick.java
+++ Assignment #4 paging/src/RandomPick.java
@@ -1,26 +1,35 @@
 import java.util.Random;
 
-
 /**
- * Called with an int and returns an int based on the assignment specifications.
+ * Called with an int representing the reference page and returns an int
+ * representing the next page with 70% probability the page returned is self or
+ * neighbour.
+ * 
  * @author Theta(1)
  *
  */
-public class RandomPick{
+public class RandomPick {
 	private final static int VMEM = 10;
 	private final static int LOCALITY_PROBABILITY = 7;
-	public static int pickAPage(int i){
+
+	/**
+	 * * Called with an int representing the reference page and returns an int
+	 * representing the next page with 70% probability the page returned is self
+	 * or neighbour.
+	 * 
+	 * @param i
+	 *            is an int representing the reference page number
+	 * @return an int representing the new page
+	 */
+	public static int pickAPage(int i) {
 		Random random = new Random();
 		int probability = random.nextInt(VMEM);
 		int locality;
 		if (probability < LOCALITY_PROBABILITY)
 			locality = random.nextInt((1 - -1) + 1) + 1;
-		else 
+		else
 			locality = random.nextInt((8 - 2) + 1) + 2;
-		return (i+locality)%VMEM;
+		return (i + locality) % VMEM;
 	}
-	
 
 }
-
-
