aceaee7a553689159688a9b7e85303978d116a5d
David <de.thorpe@gmail.com>
2015-03-16 15:30:19 -0700

    First implimentation of FIFO. Averages seem low

--- Assignment #4 paging/src/RandomPick.java
+++ Assignment #4 paging/src/RandomPick.java
@@ -9,27 +9,28 @@ import java.util.Random;
  *
  */
 public class RandomPick {
-	private final static int VMEM = 10;
+	private final static int VIRTUAL_MEM_SIZE = 10;
 	private final static int LOCALITY_PROBABILITY = 7;
-
+	static Random random = new Random();
 	/**
-	 * * Called with an int representing the reference page and returns an int
-	 * representing the next page with 70% probability the page returned is self
-	 * or neighbour.
+	 * There is a 70% probability that the next reference will be to page i,
+	 * i-1, or i+1. Otherwise, the return should be larger. Page references
+	 * should wrap from 9 back to 0.
 	 * 
 	 * @param i
 	 *            is an int representing the reference page number
 	 * @return an int representing the new page
 	 */
 	public static int pickAPage(int i) {
-		Random random = new Random();
-		int probability = random.nextInt(VMEM);
+		
+		int probability = random.nextInt(VIRTUAL_MEM_SIZE);
 		int locality;
-		if (probability < LOCALITY_PROBABILITY)
-			locality = random.nextInt((1 - -1) + 1) + 1;
-		else
+		if (probability < LOCALITY_PROBABILITY){
+			locality = random.nextInt((1 - -1) + 1) + -1;
+		}
+		else{
 			locality = random.nextInt((8 - 2) + 1) + 2;
-		return (i + locality) % VMEM;
+		}
+		return (i + locality) % VIRTUAL_MEM_SIZE;
 	}
-
 }
