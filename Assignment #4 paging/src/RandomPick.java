7e033c2dee235023b83a300d9f4be33f43fe9359
David <de.thorpe@gmail.com>
2015-03-17 16:42:35 -0700

    Made modulus work right

--- Assignment #4 paging/src/RandomPick.java
+++ Assignment #4 paging/src/RandomPick.java
@@ -10,12 +10,11 @@ import java.util.Random;
  */
 public class RandomPick {
 	private final static int LOCALITY_PROBABILITY = 7;
-	static Random random = new Random();
+	static int local;
 
 	/**
 	 * There is a 70% probability that the next reference will be to page i,
-	 * i-1, or i+1. Otherwise, the return should be larger. Page references
-	 * should wrap from 9 back to 0.
+	 * i-1, or i+1. Otherwise, the return should be between 2 and 8.
 	 * 
 	 * @param i
 	 *            is an int representing the reference page number
@@ -24,13 +23,16 @@ public class RandomPick {
 	 * @return an int representing the new page
 	 */
 	public static int pickAPage(int i, int virtualMemorySize) {
-		int probability = random.nextInt(virtualMemorySize);
+		Random random = new Random();
+		int probability = random.nextInt(10);
 		int locality;
 		if (probability < LOCALITY_PROBABILITY) {
 			locality = random.nextInt((1 - -1) + 1) + -1;
 		} else {
 			locality = random.nextInt((8 - 2) + 1) + 2;
 		}
-		return (i + locality) % virtualMemorySize;
+		//c derived languages: -1 % 9 = -1
+		//this allows for -1 % 9 = 8
+		return ((((i + locality) % virtualMemorySize) + virtualMemorySize) % virtualMemorySize);
 	}
 }
