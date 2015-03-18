deeb5b284843f7237a9a580a28bacd03dd59fa32
David <de.thorpe@gmail.com>
2015-03-17 23:01:19 -0700

    Added check for open spaces

--- Assignment #4 paging/src/LRU.java
+++ Assignment #4 paging/src/LRU.java
@@ -7,15 +7,21 @@
  */
 public class LRU extends PageReplacementAlgorithm {
 
-	protected int replacePage(int executionMarker, int nextPage) {
+	protected int replacePage(int executionMarker, int nextPage, int time) {
 		Page page = physicalMemory[0];
-		for (int i = 1; i > physicalMemory.length; i++) {
-			if (physicalMemory[i] != null && page.getTimeLastUsed() < physicalMemory[i].getTimeLastUsed()) {
-				page = this.physicalMemory[i];
+		for (int i = 1; i < physicalMemory.length; i++) {
+			if (physicalMemory[i] != null) {
+				if (page.getTimeLastUsed() > physicalMemory[i]
+						.getTimeLastUsed()) {
+					page = this.physicalMemory[i];
+					executionMarker = i;
+				}
+			} else {
 				executionMarker = i;
+				break;
 			}
 		}
-		physicalMemory[executionMarker] = new Page(nextPage, 0);
+		physicalMemory[executionMarker] = new Page(nextPage, time);
 		return executionMarker;
 	}
 }
