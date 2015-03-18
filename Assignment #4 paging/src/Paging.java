2c32351be5d543a0852646c110000a5c082c2a1c
David <de.thorpe@gmail.com>
2015-03-17 22:16:10 -0700

    Single test case

--- Assignment #4 paging/src/Paging.java
+++ Assignment #4 paging/src/Paging.java
@@ -7,24 +7,23 @@ public class Paging {
 		
 		//run each 5 times
 		FIFO fifo = new FIFO();
-		LFU lfu = new LFU();
-		LRU lru = new LRU();
-		MFU mfu = new MFU();
+//		LFU lfu = new LFU();
+//		LRU lru = new LRU();
+//		MFU mfu = new MFU();
 		
 		//returns the hit ratio
 		double h1 = fifo.run(); 
-		double h2 = lfu.run(); 
-
-		double h3 = lru.run(); 
-		double h4 = mfu.run(); 
+//		double h2 = lfu.run(); 
+//		double h3 = lru.run(); 
+//		double h4 = mfu.run(); 
 
 		
 		//print average hit ratio
 		Print.thisStringln("--Average hit ratios--");
 		Print.thisStringln("FIFO: "+h1);
-		Print.thisStringln("LFU:  "+h2);
-		Print.thisStringln("LRU:  "+h3);
-		Print.thisStringln("MFU:  "+h4);
+//		Print.thisStringln("LFU:  "+h2);
+//		Print.thisStringln("LRU:  "+h3);
+//		Print.thisStringln("MFU:  "+h4);
 		
 
 	}
