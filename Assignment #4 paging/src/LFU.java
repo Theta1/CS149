a262de7ba4975f9c9a1df6dba42f36c763be127a
David <de.thorpe@gmail.com>
2015-03-17 22:36:43 -0700

    Fixed single page paging because of null

--- Assignment #4 paging/src/LFU.java
+++ Assignment #4 paging/src/LFU.java
@@ -10,10 +10,14 @@ public class LFU extends PageReplacementAlgorithm {
 	protected int replacePage(int executionMarker, int nextPage) {
 		Page page = physicalMemory[0];
 		for (int i = 1; i < physicalMemory.length; i++) {
-			if (physicalMemory[i] != null
-					&& page.getHitCount() > physicalMemory[i].getHitCount()) {
-				page = this.physicalMemory[i];
+			if (physicalMemory[i] != null) {
+				if (page.getHitCount() > physicalMemory[i].getHitCount()) {
+					page = this.physicalMemory[i];
+					executionMarker = i;
+				}
+			}else{
 				executionMarker = i;
+				break;
 			}
 		}
 		this.physicalMemory[executionMarker] = new Page(nextPage, 0);
