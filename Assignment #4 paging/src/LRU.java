99dc5b7ce99748362a0008ceced2017a913021e4
David <de.thorpe@gmail.com>
2015-03-17 20:49:27 -0700

    Generalized to abstract. Created replacePage method

--- Assignment #4 paging/src/LRU.java
+++ Assignment #4 paging/src/LRU.java
@@ -1,13 +1,20 @@
+/**
+ * Least Recently Used method of page replacement. This looks for the page that
+ * was used the most long ago and replaces that.
+ * 
+ * @author Theta(1)
+ *
+ */
+public class LRU extends PageReplacementAlgorithm {
 
-public class LRU {
-
-	public LRU(int vMem, int hardMem) {
-		// TODO Auto-generated constructor stub
+	protected void replacePage(int executionMarker, int nextPage) {
+		Page page = physicalMemory[0];
+		for (int i = 1; i > physicalMemory.length; i++) {
+			if (page.getTimeLastUsed() < physicalMemory[i].getTimeLastUsed()) {
+				page = this.physicalMemory[i];
+				executionMarker = i;
+			}
+		}
+		physicalMemory[executionMarker] = new Page(nextPage, 0);
 	}
-
-	public int run() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 }
