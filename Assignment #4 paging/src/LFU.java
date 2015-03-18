a61124688f5b4acfebed9d1faacece9f876b13cb
David <de.thorpe@gmail.com>
2015-03-17 20:49:03 -0700

    Generalized to abstract. Created replacePage method

--- Assignment #4 paging/src/LFU.java
+++ Assignment #4 paging/src/LFU.java
@@ -1,54 +1,17 @@
+/**
+ * @author Theta(1)
+ *
+ */
+public class LFU extends PageReplacementAlgorithm {
 
-public class LFU {
-
-	private static final int RUN_TIME_NUMBER = 5;
-	private static final int REFERENCE_NUMBER = 100;
-	private int[] physicalMemory;
-	private int physicalMemorySize;
-	private int virtualMemorySize;
-
-	public LFU(int vMem, int hardMem) {
-		this.virtualMemorySize=vMem;
-		this.physicalMemorySize=hardMem;
-	}
-
-	public double run() {
-		double averageHitRatio = 0;
-		// Number of times run
-		for (int i = 0; i < this.RUN_TIME_NUMBER; i++) {
-			int hitRatio = 0;
-			// clears mem
-			this.physicalMemory = new int[physicalMemorySize];
-			// starts at the first index
-			int indexMarker = 0;
-			// loads first index from virtual mem
-			this.physicalMemory[indexMarker] = 0;
-			// Number of page references each run
-			for (int j = 0; j < this.REFERENCE_NUMBER; j++) {
-				// finds the next page randomly
-				int nextPage = RandomPick.pickAPage(
-						this.physicalMemory[indexMarker
-								% this.physicalMemorySize], this.virtualMemorySize);
-				// if the new page is not in memory
-				if (this.pageNotInMemory(nextPage)) {
-					// put it there and advance the index.
-					this.physicalMemory[++indexMarker % this.physicalMemorySize] = nextPage;
-				} else {
-					// increment the hit ratio
-					hitRatio++;
-				}
+	protected void replacePage(int executionMarker, int nextPage) {
+		Page page = physicalMemory[0];
+		for (int i = 1; i < physicalMemory.length; i++) {
+			if (page.getHitCount() > physicalMemory[i].getHitCount()) {
+				page = this.physicalMemory[i];
+				executionMarker = i;
 			}
-			// Store a fraction of the hitRatio average.
-			averageHitRatio += hitRatio / this.RUN_TIME_NUMBER;
 		}
-		// return full average
-		return averageHitRatio;
+		this.physicalMemory[executionMarker] = new Page(nextPage, 0);
 	}
-
-	private boolean pageNotInMemory(int nextPage) {
-		// TODO Auto-generated method stub
-		return false;
-
-	}
-
 }
