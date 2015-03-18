5e03ea91db6e6a7a9c7b24593d2b2b466efbbfe0
David <de.thorpe@gmail.com>
2015-03-17 22:14:24 -0700

    FIFO now works on seperate marker

--- Assignment #4 paging/src/FIFO.java
+++ Assignment #4 paging/src/FIFO.java
@@ -1,13 +1,16 @@
 /**
+ * First in First Out method of page replacement. This replaces the page the has
+ * been in the physical memory the longest.
+ * 
  * @author Theta(1)
  *
  */
-public class FIFO extends PageReplacementAlgorithm{
-
-	protected void replacePage(int executionMarker, int nextPage) {	
+public class FIFO extends PageReplacementAlgorithm {
+	protected int replacePage(int executionMarker, int nextPage) {
 		// Advance the index and put it in
-		executionMarker = (executionMarker + 1) % PHYSICAL_MEMORY_SIZE;
-		this.physicalMemory[executionMarker] = new Page(nextPage, 0);
+		marker = (marker + 1) % PHYSICAL_MEMORY_SIZE;
+		physicalMemory[marker] = new Page(nextPage, 0);
+		return marker;
 	}
 
 }
