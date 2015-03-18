dc794edf1fc4864bca5208736855cae4dc43efa7
David <de.thorpe@gmail.com>
2015-03-17 20:51:44 -0700

    used default constructors. Moved parameters into abstract method.

--- Assignment #4 paging/src/Paging.java
+++ Assignment #4 paging/src/Paging.java
@@ -6,10 +6,10 @@ public class Paging {
 		int hardMem = 4;
 		
 		//run each 5 times
-		FIFO fifo = new FIFO(vMem, hardMem);
-		LFU lfu = new LFU(vMem, hardMem);
-		LRU lru = new LRU(vMem, hardMem);
-		MFU mfu = new MFU(vMem, hardMem);
+		FIFO fifo = new FIFO();
+		LFU lfu = new LFU();
+		LRU lru = new LRU();
+		MFU mfu = new MFU();
 		
 		//returns the hit ratio
 		double h1 = fifo.run(); 
@@ -22,7 +22,7 @@ public class Paging {
 		//print average hit ratio
 		Print.thisStringln("--Average hit ratios--");
 		Print.thisStringln("FIFO: "+h1);
-		Print.thisStringln("LFO:  "+h2);
+		Print.thisStringln("LFU:  "+h2);
 		Print.thisStringln("LRU:  "+h3);
 		Print.thisStringln("MFU:  "+h4);
 		
