2432024f41dec5834c09326dbbd1545a339c2c85
David <de.thorpe@gmail.com>
2015-03-17 22:37:46 -0700

    bla

--- Assignment #4 paging/src/Paging.java
+++ Assignment #4 paging/src/Paging.java
@@ -6,22 +6,22 @@ public class Paging {
 		int hardMem = 4;
 		
 		//run each 5 times
-		FIFO fifo = new FIFO();
-//		LFU lfu = new LFU();
+//		FIFO fifo = new FIFO();
+		LFU lfu = new LFU();
 //		LRU lru = new LRU();
 //		MFU mfu = new MFU();
 		
 		//returns the hit ratio
-		double h1 = fifo.run(); 
-//		double h2 = lfu.run(); 
+//		double h1 = fifo.run(); 
+		double h2 = lfu.run(); 
 //		double h3 = lru.run(); 
 //		double h4 = mfu.run(); 
 
 		
 		//print average hit ratio
 		Print.thisStringln("--Average hit ratios--");
-		Print.thisStringln("FIFO: "+h1);
-//		Print.thisStringln("LFU:  "+h2);
+//		Print.thisStringln("FIFO: "+h1);
+		Print.thisStringln("LFU:  "+h2);
 //		Print.thisStringln("LRU:  "+h3);
 //		Print.thisStringln("MFU:  "+h4);
 		
