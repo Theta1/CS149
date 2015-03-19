ecb7b33d14bba2c8437781bf111ba81311d29066
David <de.thorpe@gmail.com>
2015-03-19 13:51:35 -0700

    Added constructor call to superclass

--- Assignment #4 paging/src/LFU.java
+++ Assignment #4 paging/src/LFU.java
@@ -7,6 +7,23 @@
  */
 public class LFU extends PageReplacementAlgorithm {
 
+	/**
+	 * General constructor.
+	 * 
+	 * @param runTimeNumber
+	 *            is the number of times to run.
+	 * @param numberOfReferences
+	 *            is the number of references to make in each run.
+	 * @param physicalMemorySize
+	 *            is the number of slots in physical memory.
+	 * @param virtualMemorySize
+	 *            is the number of slots in virtual memory.
+	 */
+	LFU(int runTimeNumber, int numberOfReferences, int physicalMemorySize,
+			int virtualMemorySize) {
+		super(runTimeNumber, numberOfReferences, physicalMemorySize, virtualMemorySize);
+	}
+
 	protected int replacePage(int executionMarker, int nextPage, int time) {
 		Page page = physicalMemory[0];
 		for (int i = 1; i < physicalMemory.length; i++) {
