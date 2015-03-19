ea4737ccaaf6645c20c7f0d81ca4b2d841ef1eec
David <de.thorpe@gmail.com>
2015-03-19 13:51:53 -0700

    Added constructor call to superclass

--- Assignment #4 paging/src/LRU.java
+++ Assignment #4 paging/src/LRU.java
@@ -7,6 +7,24 @@
  */
 public class LRU extends PageReplacementAlgorithm {
 
+	/**
+	 * General constructor.
+	 * 
+	 * @param runTimeNumber
+	 *            is the number of times to run.
+	 * @param numberOfReferences
+	 *            is the number of references to make in each run.
+	 * @param physicalMemorySize
+	 *            is the number of slots in physical memory.
+	 * @param virtualMemorySize
+	 *            is the number of slots in virtual memory.
+	 */
+	LRU(int runTimeNumber, int numberOfReferences, int physicalMemorySize,
+			int virtualMemorySize) {
+		super(runTimeNumber, numberOfReferences, physicalMemorySize,
+				virtualMemorySize);
+	}
+
 	protected int replacePage(int executionMarker, int nextPage, int time) {
 		Page page = physicalMemory[0];
 		for (int i = 1; i < physicalMemory.length; i++) {
