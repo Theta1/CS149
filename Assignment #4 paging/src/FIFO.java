6a396f33af0507311f4efe4d39de8c5435a4a52e
David <de.thorpe@gmail.com>
2015-03-19 13:51:19 -0700

    Added constructor call to superclass

--- Assignment #4 paging/src/FIFO.java
+++ Assignment #4 paging/src/FIFO.java
@@ -6,9 +6,28 @@
  *
  */
 public class FIFO extends PageReplacementAlgorithm {
+
+	/**
+	 * General constructor.
+	 * 
+	 * @param runTimeNumber
+	 *            is the number of times to run.
+	 * @param numberOfReferences
+	 *            is the number of references to make in each run.
+	 * @param physicalMemorySize
+	 *            is the number of slots in physical memory.
+	 * @param virtualMemorySize
+	 *            is the number of slots in virtual memory.
+	 */
+	FIFO(int runTimeNumber, int numberOfReferences, int physicalMemorySize,
+			int virtualMemorySize) {
+		super(runTimeNumber, numberOfReferences, physicalMemorySize,
+				virtualMemorySize);
+	}
+
 	protected int replacePage(int executionMarker, int nextPage, int time) {
 		// Advance the index and put it in
-		marker = (marker + 1) % PHYSICAL_MEMORY_SIZE;
+		marker = (marker + 1) % physicalMemorySize;
 		physicalMemory[marker] = new Page(nextPage, time);
 		return marker;
 	}
