9d2b6c2915d49db75d8f5264c7006fcc5533d009
David <de.thorpe@gmail.com>
2015-03-19 13:54:01 -0700

    Created global variables and passed to constructors

--- Assignment #4 paging/src/Paging.java
+++ Assignment #4 paging/src/Paging.java
@@ -1,14 +1,25 @@
+/**
+ * 
+ * @author Theta(1)
+ *
+ */
 public class Paging {
+	private final static int RUN_TIME_NUMBER = 5;
+	private final static int NUMBER_OF_REFERENCES = 100;
+	private final static int PHYSICAL_MEMORY_SIZE = 4;
+	private final static int VIRTUAL_MEMORY_SIZE = 10;
 
 	public static void main(String[] args) {
-		int vMem = 10;
-		int hardMem = 4;
 
 		// run each 5 times
-		FIFO fifo = new FIFO();
-		LFU lfu = new LFU();
-		LRU lru = new LRU();
-		MFU mfu = new MFU();
+		FIFO fifo = new FIFO(RUN_TIME_NUMBER, NUMBER_OF_REFERENCES,
+				PHYSICAL_MEMORY_SIZE, VIRTUAL_MEMORY_SIZE);
+		LFU lfu = new LFU(RUN_TIME_NUMBER, NUMBER_OF_REFERENCES,
+				PHYSICAL_MEMORY_SIZE, VIRTUAL_MEMORY_SIZE);
+		LRU lru = new LRU(RUN_TIME_NUMBER, NUMBER_OF_REFERENCES,
+				PHYSICAL_MEMORY_SIZE, VIRTUAL_MEMORY_SIZE);
+		MFU mfu = new MFU(RUN_TIME_NUMBER, NUMBER_OF_REFERENCES,
+				PHYSICAL_MEMORY_SIZE, VIRTUAL_MEMORY_SIZE);
 
 		// returns the hit ratio
 		double h1 = fifo.run();
@@ -19,9 +30,8 @@ public class Paging {
 		// print average hit ratio
 		Print.thisStringln("--Average hit ratios--");
 		Print.thisStringln("FIFO: " + h1);
-		Print.thisStringln("LFU:  " + h2);
+		Print.thisStringln("LFO:  " + h2);
 		Print.thisStringln("LRU:  " + h3);
 		Print.thisStringln("MFU:  " + h4);
-
 	}
 }
