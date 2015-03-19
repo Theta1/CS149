dc0d85fa7a9b0295556a06e6f37339b367932e40
Nate Kong <NateWKong@gmail.com>
2015-03-18 22:02:14 -0700

    changed print

--- Assignment #4/src/FirstFit.java
+++ Assignment #4/src/FirstFit.java
@@ -11,8 +11,8 @@ import java.util.ArrayList;
 
 public class FirstFit {
 	private static ArrayList<Process> processes;
-	private int maxMem;
-	private int[]ff;
+	private static int maxMem;
+	private static int[]ff;
 	private static int mbCounter;
 	private static int time;
 	
@@ -33,7 +33,7 @@ public class FirstFit {
 			Process p = processes.get(mbCounter);
 			
 			//get first empty location
-			int location = findFirstEmpty();
+			int location = findFirstEmpty(p.getSize());
 			//add process
 			if(location < 0) {
 				addProcess(p, location);
@@ -49,7 +49,49 @@ public class FirstFit {
 		return mbCounter;
 	}
 	
-	public static int findFirstEmpty() {
+	/**
+	 * finds the first empty location in the array
+	 * @param size the needed to fill the process
+	 * @return the first location in the array
+	 * to fill the array
+	 */
+	public static int findFirstEmpty(int size) {
+		int start = 0;
+		int end = -1;
+		
+		for(int i = 0; i < maxMem; i++) {
+			if (ff[i] == 0) {
+				if (start > end)
+				{	
+					start = i;
+					end = i;
+				}
+				else
+				{	end = i;	}
+			}
+			else {
+				start = i;
+			}
+				
+			
+			if ((end - start) <= size) {
+				return start;
+			}
+		}
+
 		return -1;
+	}
+	
+	/**
+	 * 
+	 * @param p
+	 * @param start
+	 */
+	public static void addProcess( Process p, int start) {
+		for (; start < p.getSize(); start++) {
+			ff[start] = p.getName();
+		}
+		
+		Print.printAdd(p);
 	}
 }
--- Assignment #4/src/Print.java
+++ Assignment #4/src/Print.java
@@ -11,8 +11,7 @@ public class Print {
 
 	
 	/**
-	 * Each time a process is swapped in, or a process completes and therefore
-	 * is removed from memory, print a memory map, e.g., [ 1][ 1][ 1][ 1][ 1][..]
+	 * Prints a memory map, e.g., [ 1][ 1][ 1][ 1][ 1][..]
 	 * [..][..][..][10][10][10][10][10][..][ 3][ 3][ 3]
 	 * where the characters are the process names (one character per MB) and the
 	 * dots are holes ([..] per MB). Indicate which process entered or left. For
@@ -23,29 +22,34 @@ public class Print {
 	 * @param process
 	 *            is the process that has been added or removed.
 	 */
-	void printMap(ArrayList<Integer> mem, Process process) {
-		boolean processAdded = false;
+	public static void printMap(ArrayList<Integer> mem, Process process) {
 		System.out.print("[");
 		for(int i: mem)
 		{	
 			if (i == mem.get(mem.size()-1))
 			{	System.out.println(i + "]");	}
 			else if (i == 0)
-			{	System.out.print("..");	}
+			{	System.out.print(".");	}
 			else
-			{	
-				System.out.printf("%2d",i);
-				if (i == process.getName()){
-					processAdded = true;
-				}
-			}
+			{	System.out.println(process.getName());		}
 		}
-		if (processAdded){
-			System.out.printf(" Added: %2d of size " + process.getSize() 
-					+ " and durration "	+ process.getDuration(), 
-					process.getName());
-		}else{
-			System.out.printf(" Removed: %2d\n", process.getName());
-		}
+		
+	}
+	
+	/**
+	 * Prints the process thats added with stats
+	 * @param process
+	 */
+	public static void printAdd(Process process) {
+		System.out.println("Added: "+ process.getName() + " of size " + process.getSize() 
+					+ " and durration "	+ process.getDuration()	);		
+	}
+	
+	/**
+	 * Prints the process that is completed
+	 * @param process
+	 */
+	public static void printRemove(Process process) {
+		System.out.println("Removed: Process " + process.getName() );
 	}
 }
